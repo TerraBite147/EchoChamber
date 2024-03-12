baa53ff126e3c714f190b36c1c4b3e5807a015ba
Logan  Carlow <logan.carlow@gmail.com>
2024-03-12 20:13:32 +0000

    feat(base): Add link to admin center

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from . import views
 from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
     path("", views.BlogList.as_view(), name="home"),
     path("post/<slug:slug>/", views.post_detail, name="post_detail"),
     path('post/<slug:slug>/like', views.like_post, name='like_post'),
-    path('post/<int:post_id>/like', views.like_post, name='like_post'),
-    path('post/<int:post_id>/unlike', views.unlike_post, name='unlike_post'),
 ]
