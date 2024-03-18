7d1833c49ee0bf89197397e8c873587818c4a2bd
Logan  Carlow <logan.carlow@gmail.com>
2024-03-18 15:03:52 +0000

    feat(post): Add delete section for posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,10 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.BlogList.as_view(), name="home"),
+    path("create/", views.create_post, name="create_post"),
     path("post/<slug:slug>/", views.post_detail, name="post_detail"),
-    path('post/<slug:slug>/like', views.like_post, name='like_post'),
-    path('like_comment/<int:comment_id>/', like_comment, name='like_comment'),
-    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
-    path('create/', views.create_post, name='create_post'),
-    
+    path("post/<slug:slug>/like", views.like_post, name="like_post"),
+    path("delete_post/<slug:slug>/", views.delete_post, name="delete_post"),
+    path("like_comment/<int:comment_id>/", like_comment, name="like_comment"),
+    path("delete_comment/<int:comment_id>/", views.delete_comment, name="delete_comment"),
 ]
