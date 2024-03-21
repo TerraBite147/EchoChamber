abf50a422e8b799e94581233060de091a668b949
Logan  Carlow <logan.carlow@gmail.com>
2024-03-21 11:22:55 +0000

    feat(sort and filter): adjust drop down to show what is selected

--- static/js/infinite-scroll.js
+++ static/js/infinite-scroll.js
@@ -1,11 +1,10 @@
 $(function() {
-    var $loader = $('#loader'); // The loader element
-    var page = 2; // Start from the second page
-    var isLoading = false; // To prevent multiple loads
-    var sort = new URLSearchParams(window.location.search).get('sort') || 'date'; // Get the current sort parameter or default to 'date'
-    var category = new URLSearchParams(window.location.search).get('category'); // Get the current category parameter
+    var $loader = $('#loader');
+    var page = 2;
+    var isLoading = false;
+    var sort = new URLSearchParams(window.location.search).get('sort') || 'date';
+    var category = new URLSearchParams(window.location.search).get('category');
 
-    // Detect when the user scrolls near the bottom of the content
     $(window).scroll(function() {
         var endOfPage = $(document).height() - $(window).height() - $loader.height();
         
@@ -13,24 +12,21 @@ $(function() {
             isLoading = true;
             $loader.show();
 
-            // Construct the URL with the page, sort, and category parameters
-            var ajaxUrl = '/?page=' + page + '&sort=' + sort;
+            var ajaxUrl = `/?page=${page}&sort=${sort}`;
             if (category) {
-                ajaxUrl += '&category=' + category;
+                ajaxUrl += `&category=${category}`;
             }
 
             $.ajax({
                 url: ajaxUrl,
                 type: 'GET',
                 success: function(data) {
-                    // Append the new posts to the content
                     if (data.html) {
                         $('#posts-container').append(data.html);
                         page++;
                         isLoading = false;
                         $loader.hide();
                     } else {
-                        // No more posts to load
                         $loader.hide();
                     }
                 },
